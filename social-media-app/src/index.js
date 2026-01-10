71f6198786931878b05122db9c5929e570e68fb0
Mrityunjoy Bhaduree <mrityunjoy.zicore@gmail.com>
2026-01-10 11:49:43 +0600

    change main to min.

--- social-media-app/src/index.js
+++ social-media-app/src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
