c22d0ebf8d5654802929f223cf8721ccff71b1d8
Mrityunjoy Bhaduree <mrityunjoy.zicore@gmail.com>
2026-01-13 17:45:57 +0600

    update home file

--- social-media-app/src/pages/Home.jsx
+++ social-media-app/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use } from "react";
 import Layout from "../components/Layout";
 import {Row, Col, Image } from "react-bootstrap";
 import { randomAvatar } from "../utils";
@@ -6,11 +6,18 @@ import useSWR from "swr";
 import { fetcher } from "../helpers/axios";
 import { getUser } from "../hooks/user.actions";
 import CreatePost from "../components/posts/CreatePost";
+import Post from "../components/posts/Post";
+import { API_VERSION } from "../config/api";
+
 
 
 
 function Home () {
     const user = getUser();
+
+    const posts = useSWR(`${API_VERSION}/post/`, fetcher, {
+        refreshInterval: 10000,
+    });
 
     if (!user) {
         return <div>Loading</div>
@@ -32,8 +39,14 @@ function Home () {
                             />
                         </Col>
                         <Col sm={10} className="flex-grow-1">
-                            <CreatePost />
+                            <CreatePost refresh={posts.mutate}/>
                         </Col>
+                    </Row>
+                    <Row className="my-4">
+                        {posts.data?.results.map((post, index) => (
+                            <Post key={index} post={post}
+                                refresh={posts.mutate}  />
+                        ))}
                     </Row>
                 </Col>
             </Row>        
