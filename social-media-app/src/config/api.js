ba8b6220c909d2869e86c238d31984b814b6c5f3
Mrityunjoy Bhaduree <mrityunjoy.zicore@gmail.com>
2026-01-26 16:01:21 +0600

    update config file

--- social-media-app/src/config/api.js
+++ social-media-app/src/config/api.js
@@ -1,2 +1,2 @@
-export const API_VERSION = "/api/v1"
-export const BASE_URL = "https://full-stack-django-and-react-backend.onrender.com/api/v1";
+// export const API_VERSION = "/api/v1"
+// export const BASE_URL = "https://full-stack-django-and-react-backend.onrender.com/api/v1";
